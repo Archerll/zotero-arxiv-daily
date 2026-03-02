1d7ab0a00de9c52ac081bcecf10a8f9c4596fde1
Irving Han <39262093+Archerll@users.noreply.github.com>
2026-03-02 15:20:29 +0800

    Add 'ARXIV_QUERY' to secrets list

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import os
-secrets = ["ZOTERO_ID", "ZOTERO_KEY", "SENDER", "RECEIVER", "SENDER_PASSWORD", "OPENAI_API_KEY", "OPENAI_API_BASE"]
+secrets = ["ZOTERO_ID", "ZOTERO_KEY", "SENDER", "RECEIVER", "SENDER_PASSWORD", "OPENAI_API_KEY", "OPENAI_API_BASE", "ARXIV_QUERY"]
 for secret in secrets:
     val = os.environ.get(secret, "")
     print(f"{secret}: length={len(val)}, startswith={val[:5]}")
